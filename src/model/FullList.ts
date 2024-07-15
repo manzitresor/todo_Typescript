8e686fe1d37879d8c9c10730cf7395a03fd5dd5d
manzitresor <manzitre@gmail.com>
2024-07-15 15:14:28 +0200

    add full list item model

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -1,10 +1,44 @@
 import ListItem from "./listItems";
 
 interface List {
-    list: ListItem;
+    list: ListItem[];
     load(): void;
     save(): void;
     clearList(): void;
     addItem(itemObj: ListItem): void;
     removeItem(id: string): void;
+}
+
+export default class FullList implements List {
+
+   static instance: FullList =  new FullList();
+
+   private constructor(private _list: ListItem[] = []){}
+    get list(): ListItem[] {
+        return this._list;
+    }
+    load(){
+        const storedList: string | null = localStorage.getItem('myList');
+        if (storedList !== 'string')  return 
+        const parseList: {_id: string, _item: string,_checked: boolean} [] = JSON.parse(storedList);
+        parseList.forEach( itemObj => {
+            const newListItem = new ListItem(itemObj._id,itemObj._item,itemObj._checked);
+            FullList.instance.addItem(newListItem);
+        })
+    }
+    save(){
+        localStorage.setItem('myList',JSON.stringify(this.list));
+    }
+    clearList(): void {
+        this._list = [];
+        this.save();
+    }
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj);
+        this.save();
+    }
+    removeItem(id: string): void {
+        this._list =  this._list.filter(item => item.id !== id);
+        this.save();
+    }
 }
